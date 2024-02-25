2b569d637ff74d870ffbcdc56db5564101614675
PluemW <96386038+PluemW@users.noreply.github.com>
2024-02-25 18:58:46 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,132 +1,68 @@
+// #include <SD.h>                      // need to include the SD library
+// //#define SD_ChipSelectPin 53  //example uses hardware SS pin 53 on Mega2560
+// #define SD_ChipSelectPin 53  //using digital pin 4 on arduino nano 328, can use other pins
+// #include <TMRpcm.h>           //  also need to include this library...
+// #include <SPI.h>
+// #include <Tone.h>
+
+
+// TMRpcm tmrpcm;   // create an object for use in this sketch
+// File myFile;
+// Tone notePlayer[2];
+
+
+// void setup(){
+
+//   // myFile = SD.open("2");
+//   tmrpcm.speakerPin = 47; //5,6,11 or 46 on Mega, 9 on Uno, Nano, etc
+//     // notePlayer[0].begin(46);
+
+//   Serial.begin(9600);
+//   if (!SD.begin(SD_ChipSelectPin)) {  // see if the card is present and can be initialized:
+//     Serial.println("SD fail");  
+//     return;   // don't do anything more if not
+//   }
+// }
+
+
+
+// void loop(){  
+//   // notePlayer[0].play(myFile);
+//   tmrpcm.setVolume(10);
+//   tmrpcm.play("2.wav");
+//   Serial.println("123");
+// }
+// Duelling Tones - Simultaneous tone generation.
+// To mix the output of the signals to output to a small speaker (i.e. 8 Ohms or higher),
+// simply use 1K Ohm resistors from each output pin and tie them together at the speaker.
+// Don't forget to connect the other side of the speaker to ground!
+
+// This example plays notes 'a' through 'g' sent over the Serial Monitor.
+// 's' stops the current playing tone.  Use uppercase letters for the second.
+
+#include <Tone.h>
 #include <Arduino.h>
-#include <LiquidCrystal_I2C.h>
-#include <MQTT.h>
-#include <WiFi.h>
+int notes[] = { NOTE_A3,
+                NOTE_B3,
+                NOTE_C4,
+                NOTE_D4,
+                NOTE_E4,
+                NOTE_F4,
+                NOTE_G4 };
 
-#define Button_Pin   35
-#define Vol     23
-String height_msg;
-boolean sent = false;
+// You can declare the tones as an array
+Tone notePlayer[2];
 
-LiquidCrystal_I2C lcd(0x27, 16, 2); // set up LCD-I2C 16 Col 2 Row
-WiFiClient net;
-MQTTClient client;
-
-// Config wifi path
-const char ssid[] = "SSID";
-const char pass[] = "WIFI_PASSWORD";
-
-// Config sever path
-const char mqtt_broker[]="test.mosquitto.org";
-const char mqtt_topic[]="group/command";
-const char mqtt_client_id[]="arduino_group_x"; // must change this string to a unique value
-int MQTT_PORT=1883;
-
-// Function Topic
-void connect();
-void messageReceived(String &topic, String &payload);
-void IRAM_ATTR IO_INT_ISR();
-
-void setup(){
-  pinMode(Vol,INPUT);
-  pinMode(Button_Pin,INPUT);
-  attachInterrupt(Button_Pin, IO_INT_ISR, RISING);
-	lcd.init();
-	lcd.backlight();
-  WiFi.begin(ssid, pass);
-  client.begin(mqtt_broker, MQTT_PORT, net);
-  client.onMessage(messageReceived);
-  connect();
-}
-
-void loop(){
-  // Interface running
-  lcd.setCursor(3,0);
-  lcd.print("PID Tuning");
-  if(!sent){
-    lcd.setCursor(0,1);
-    lcd.print("Height(cm): ");
-    lcd.setCursor(12,1);
-    lcd.print(map(analogRead(27), 0, 4095, 0, 100));
-  }
-  delay(200);
-  lcd.clear();
-}
-
-// Function detail
-void connect()
+void setup(void)
 {
-  // Connect Wifi screen
-  int i = 0;
-  if(WiFi.status() != WL_CONNECTED){
-    lcd.setCursor(1,0);
-    lcd.print("ConnectingWIFI");
-  }
-  else if(WiFi.status() == WL_CONNECTED){
-    lcd.clear();
-    i = 0;
-    lcd.setCursor(1,0);
-    lcd.print("Connected WIFI");
-    delay(500);
-    lcd.clear();
-  }
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    lcd.setCursor(i,1);
-    lcd.print(".");
-    i++;
-    if(i>15){
-      i = 0;
-      lcd.clear();
-    }
-    delay(1000);
-  }
-  // Connect client
-  if(!client.connect(mqtt_client_id)){
-    lcd.setCursor(0,0);
-    lcd.print("ConnectingClient");
-  }
-  else if(client.connect(mqtt_client_id)){
-    lcd.clear();
-    i = 0;
-    lcd.setCursor(0,0);
-    lcd.print("Connected Client");
-    delay(500);
-    lcd.clear();
-  }
-  while (!client.connect(mqtt_client_id))
-  {
-    lcd.setCursor(i,1);
-    lcd.print(".");
-    i++;
-    if(i>15){
-      i = 0;
-      lcd.clear();
-    }
-    delay(1000);
-  }
-  client.subscribe(mqtt_topic);
+  // Serial.begin(9600);
+  notePlayer[0].begin(47);
+  notePlayer[1].begin(12);
 }
 
-void messageReceived(String &topic, String &payload)
+void loop(void)
 {
-  if(payload==height_msg){
-    lcd.clear();
-    lcd.setCursor(6,1);
-    lcd.print("Done");
-    sent = false;
-  }
-  // Note: Do not use the client in the callback to publish, subscribe or
-  // unsubscribe as it may cause deadlocks when other things arrive while
-  // sending and receiving acknowledgments. Instead, change a global variable,
-  // or push to a queue and handle it in the loop after calling `client.loop()`.
-}
+  char c='g';
+        notePlayer[0].play(notes[c - 'a']);
 
-void IRAM_ATTR onTimer(){
-  sent = true;
-  height_msg = String(map(analogRead(27), 0, 4095, 0, 100));
-  client.publish(mqtt_topic, height_msg);
-  lcd.clear();
-  lcd.setCursor(5,0);
-  lcd.print("Tuning");
 }
